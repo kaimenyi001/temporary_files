a4093527e1e9f970b4a36d4d79b9b6470ee965a0
kaimenyi001 <133211709+kaimenyi001@users.noreply.github.com>
2023-10-03 05:42:53 +0300

    Create check_cycle.c

---
+++ check_cycle.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has
+ * a cycle in it
+ * @list: pointer to the list
+ * Return: 0 if there is no cycle,
+ * 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *i;
+	listint_t *j;
+
+	i = list;
+	j = list;
+	while (list && i && j->next)
+	{
+		list = list->next;
+		i = i->next->next;
+
+		if (list == i)
+		{
+			list = j;
+			j =  i;
+			while (1)
+			{
+				i = j;
+				while (i->next != list && i->next != j)
+				{
+					i = i->next;
+				}
+				if (i->next == list)
+					break;
+
+				list = list->next;
+			}
+			return (1);
+		}
+	}
+
+	return (0);
+}
